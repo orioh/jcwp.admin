b74f7fc0002d8da42c25fcf453b813c5e2cbf2a3
John Chuang <wchih.john@gmail.com>
2020-10-29 23:33:02 +0800

    fix import warning

---
+++ src/declare.d.ts
@@ -0,0 +1,8 @@
+/**
+ * https://stackoverflow.com/questions/49258849/could-not-find-a-declaration-file-for-module-vue-xxx
+ * to prevent warning propmt of import from 'js' module
+ */
+
+declare module 'vue-monaco'
+
+declare module 'vue-power-drag'
